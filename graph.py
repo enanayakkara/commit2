d49dad96d08e8c17db2fa3877670c05481ae687a
Eranga Nanayakkara <erangananayakkara94@gmail.com>
2024-04-04 23:13:06 +0530

    commit

--- graph.py
+++ graph.py
@@ -2,7 +2,7 @@ import os
 from random import randint 
 
 for i in range(750, 1820):
-    for j in range(0, randint(1, 10)):
+    for j in range(0, randint(1, 15)):
 
         d = str(i) + ' days ago'
 
